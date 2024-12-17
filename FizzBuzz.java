69da5182334e17dd77d1dccb3e8aac8ed7995496
Dooques <74371077+Dooques@users.noreply.github.com>
2024-12-17 16:48:54 +0000

    remove second for loop and adjusted if-then-else conditions to match new format using i

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,30 +2,28 @@ import java.util.*;
 
 public class FizzBuzz {
   public static void main(String[] args) {
-    System.out.println(fizzbuzz(100));
+    System.out.println(fizzbuzz(16));
   }
 
   public static ArrayList fizzbuzz(int n) 
   {
     // Write your code here
-    ArrayList<Object> fizzbuzzList = new ArrayList<>();
+    ArrayList<String> fizzbuzzList = new ArrayList<>();
 
     for (int i = 1; i <= n; i++) {
-      fizzbuzzList.add(String.valueOf(i));
-    }
-    for (Object item : fizzbuzzList) {
-      int itemIndex = fizzbuzzList.indexOf(item);
-      int itemInt = Integer.valueOf(item.toString());
-      String itemStr = item.toString();
-      if (itemInt % 3 == 0 && 
-      itemInt % 5 == 0 && !itemStr.equals(1) && !itemStr.equals(0)) {
-        fizzbuzzList.set(itemIndex, "FizzBuzz");
-      } else if (itemInt % 3 == 0) {
-        fizzbuzzList.set(itemIndex, "Fizz");
-      } else if (itemInt % 5 == 0) {
-        fizzbuzzList.set(itemIndex, "Buzz");
+      if (
+        i % 3 == 0 && 
+        i % 5 == 0 && 
+        i != 1 && i != 0
+      ) {
+        fizzbuzzList.add("FizzBuzz");
+      } else if (i % 3 == 0) {
+        fizzbuzzList.add("Fizz");
+      } else if (i % 5 == 0) {
+        fizzbuzzList.add("Buzz");
+      } else {
+        fizzbuzzList.add(String.valueOf(i));
       }
-      
     }
     return fizzbuzzList;
   }
